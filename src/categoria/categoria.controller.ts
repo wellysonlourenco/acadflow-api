ed9e3514351b1f82e3452c44518b81a83fd766f3
Wéllyson de Lourenço <loiz.wellyson@gmail.com>
2024-08-08 17:06:21 -0400

    Modulo Categoria

--- src/categoria/categoria.controller.ts
+++ src/categoria/categoria.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { OrderParamSchema, orderValidationPipe, PageParamSchema, pageValidatioPipe, PerPageParamSchema, perPageValidationPipe, SearchParamSchema, searchValidationPipe } from '@/schema/page-param';
+import { Body, Controller, Delete, Get, HttpCode, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
 import { CategoriaService } from './categoria.service';
 
 @Controller('categoria')
@@ -11,6 +12,65 @@ export class CategoriaController {
     @Body() body: { descricao: string }
   ) {
     return await this.categoriaService.create(body.descricao)
+  }
+
+
+  @Get(':id')
+  @HttpCode(200) // OK
+  async getCategoriaById(
+    @Param('id', ParseIntPipe) id: number
+  ) {
+    return this.categoriaService.getById(id);
+  }
+
+  @Get()
+  @HttpCode(200) // OK
+  async getCategorias(
+    @Query('page', pageValidatioPipe) page: PageParamSchema,
+    @Query('limit', perPageValidationPipe) limit: PerPageParamSchema,
+    @Query('search', searchValidationPipe) search: SearchParamSchema,
+    @Query('sort', orderValidationPipe) sort: OrderParamSchema,
+  ) {
+    const take = limit || 10;
+    const skip = limit * (page - 1);
+    const orderBy = sort;
+
+    console.log(search)
+
+    const categorias = await this.categoriaService.getCategorias(
+      take,
+      skip,
+      search,
+      orderBy
+    );
+
+    const countCategorias = await this.categoriaService.getCategoriasCount()
+
+    const totalCategorias = countCategorias || 0;
+    const porPagina = limit || 10;
+    const pagina = page || 1;
+    const totalPaginas = Math.ceil(countCategorias / limit);
+
+    return { categorias, pagina, porPagina, totalPaginas, totalCategorias }
+  }
+
+  @Patch(':id')
+  @HttpCode(200) // OK
+  //@UsePipes(CategoriaValidationPipe)
+  async updateCategoria(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: { descricao: string }
+  ) {
+    return this.categoriaService.update(id, body.descricao);
+  }
+
+
+  @Delete(':id')
+  @HttpCode(200) // OK
+  async deleteCategoria(
+    @Param('id', ParseIntPipe) id: number
+  ) {
+    return this.categoriaService.deleteCategoria(id);
   }
 
 }
--- src/categoria/categoria.service.ts
+++ src/categoria/categoria.service.ts
@@ -1,5 +1,5 @@
 import { PrismaService } from '@/prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CategoriaService {
@@ -13,6 +13,83 @@ export class CategoriaService {
                 descricao
             }
         })
+    }
+
+    async getById(id: number) {
+        await this.exists(id);
+
+        return await this.prisma.categoria.findUnique({
+            where: {
+                id
+            }
+        })
+    }
+
+    async update(id: number, descricao: string) {
+        await this.exists(id);
+
+        const categoria = await this.prisma.categoria.update({
+            where: {
+                id
+            },
+            data: {
+                descricao
+            }
+        })
+        return categoria;
+    }
+
+
+    async getCategorias(take: number, skip: number, searchString: string, orderBy: 'asc' | 'desc') {
+
+        const categorias = await this.prisma.categoria.findMany({
+            take: take || undefined,
+            skip: skip || undefined,
+            where: {
+                descricao: { contains: searchString },
+            },
+            orderBy: {
+                descricao: orderBy
+            }
+        });
+
+        return categorias;
+    }
+
+
+    async getCategoriasCount() {
+        return await this.prisma.categoria.count();
+    }
+
+
+
+    async deleteCategoria(id: number) {
+        await this.exists(id);
+
+        try {
+            await this.prisma.categoria.delete({
+                where: {
+                    id
+                }
+            })
+            return true;
+        } catch (error) {
+            throw new HttpException('Não é possível excluir a categoria, pois existem produtos associados a ela.', HttpStatus.BAD_REQUEST) // 400
+        }
+    }
+
+    async exists(id: number) {
+        if (!
+            (
+                await this.prisma.categoria.count({
+                    where: {
+                        id,
+                    },
+                })
+            )
+        ) {
+            throw new HttpException(`A categoria ${id} não existe.`, HttpStatus.NOT_FOUND); // 404
+        }
     }
 
 }
